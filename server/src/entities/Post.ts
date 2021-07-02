2ac7af142b89849abbefc2d2c0acd9ab5d6244d6
Jeremy Offori <36880398+jerh-remy@users.noreply.github.com>
2021-07-02 18:29:21 +0100

    configured graphql and implemented basic CRUD operations

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 
+  @Field(() => String)
   @Property()
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,37 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import MikroConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
+  // create DB connection
   const orm = await MikroORM.init(MikroConfig);
   await orm.getMigrator().up();
 
-  //   const post = orm.em.create(Post, { title: "My first post hahaha!" });
-  //   await orm.em.persistAndFlush(post);
+  // create express app
+  const app = express();
 
-  //   const posts = await orm.em.find(Post, {});
-  //   posts.forEach((p) => console.log(p));
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => console.error(err));
